2b9d4c6db7a7608398954069c97c68cd08599bd0
Karen <karen.bandong.2018@student.lhschools.org>
2019-03-06 18:45:50 -0500

    Phase 1 and 2 complete.

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -3,6 +3,8 @@ package org.lickingheights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Phase 1 and 2
+        int numberNeeded, pickedNumber;
+        String Username;
     }
 }
