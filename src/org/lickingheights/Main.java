21851b0bc87fc15ab494c303dacfd0c854a4a9bd
Karen <karen.bandong.2018@student.lhschools.org>
2019-03-06 18:46:59 -0500

    Scanner set up

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -1,10 +1,13 @@
 package org.lickingheights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Phase 1 and 2
         int numberNeeded, pickedNumber;
         String Username;
+        Scanner keyboard= new Scanner(System.in);
     }
 }
