828eacadfab2657e8a331651358b56e8ef992665
Karen <karen.bandong.2018@student.lhschools.org>
2019-03-06 19:59:32 -0500

    While loop used and conditionals added.

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         // Phase 1 and 2
+        Scanner keyboard= new Scanner(System.in);
         int numberNeeded, pickedNumber;
         String Username;
-        Scanner keyboard= new Scanner(System.in);
+
+
+
 
         System.out.println("Hello! Welcome to the Number Guessing Game. May I please have your name?");
         Username= keyboard.nextLine();
@@ -17,5 +20,19 @@ public class Main {
         System.out.println("Pick a number from 1-100");
         pickedNumber= keyboard.nextInt();
         numberNeeded= (int)(Math.random()*100);
+        while(pickedNumber != numberNeeded){
+            if(pickedNumber > numberNeeded){
+                System.out.println("Go down");
+                System.out.println("Pick a number from 1-100");
+                pickedNumber = keyboard.nextInt();
+            }else{
+                System.out.println("Go higher");
+                System.out.println("Pick a number from 1-100");
+                pickedNumber = keyboard.nextInt();
+            }
+
+        }
+        System.out.println("Congratulation!");
+
     }
 }
